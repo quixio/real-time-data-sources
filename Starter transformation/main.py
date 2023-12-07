301cc18a06db618d0aa1a8cc49ff249172b7a43a
Steve Rosam <srosam+quix-demos@gmail.com>
2023-12-07 14:01:35 +0000

    Edited 'main.py'

--- Starter transformation/main.py
+++ Starter transformation/main.py
@@ -6,13 +6,11 @@ import pandas as pd
 client = qx.QuixStreamingClient()
 
 topic_consumer = client.get_topic_consumer(os.environ["input"], consumer_group = "empty-transformation")
-topic_producer = client.get_topic_producer(os.environ["output"])
 
 def on_stream_received_handler(stream_consumer: qx.StreamConsumer):
 
     def on_dataframe_received_handler(stream_consumer: qx.StreamConsumer, df: pd.DataFrame):
-        stream_producer = topic_producer.get_or_create_stream(stream_id = stream_consumer.stream_id)
-        stream_producer.timeseries.buffer.publish(df)
+        print(df)
 
     def on_event_data_received_handler(stream_consumer: qx.StreamConsumer, data: qx.EventData):
         print(data)
