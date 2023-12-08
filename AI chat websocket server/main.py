a348f31b196b74e52a56b2f6fea2fc374e599b20
Steve Rosam <srosam+quix-demos@gmail.com>
2023-12-08 15:06:55 +0000

    Edited 'main.py'

--- AI chat websocket server/main.py
+++ AI chat websocket server/main.py
@@ -1,22 +1,20 @@
+from fastapi import FastAPI, WebSocket
+from starlette.websockets import WebSocketDisconnect
 import json
 import os
 import uuid
-
 import pandas as pd
 import quixstreams as qx
-from fastapi import FastAPI, WebSocket
-from starlette.websockets import WebSocketDisconnect
 
 app = FastAPI()
 
 # track the connections to our websocket
-websocket_connections = []
-websocket_connections_events = []
 websocket_connections_timeseries = []
+websocket_connections_events = []
 
 
 def on_dataframe_received_handler(_: qx.StreamConsumer, df: pd.DataFrame):
-    global websocket_connections, websocket_connections_timeseries
+    global websocket_connections_timeseries
 
     # Convert the 'timestamp' column to a datetime object
     df['datetime'] = pd.to_datetime(df['timestamp'], unit='ns')
@@ -24,19 +22,17 @@ def on_dataframe_received_handler(_: qx.StreamConsumer, df: pd.DataFrame):
     # Convert the datetime object to a UTC timestamp
     df['datetime'] = df['datetime'].dt.tz_localize('UTC')
 
-    if len(websocket_connections) + len(websocket_connections_timeseries) > 0:
+    if len(websocket_connections_timeseries) > 0:
         for _, row in df.iterrows():
             data = row.to_json()
-            for ws in websocket_connections:
-                ws.send_text(data)
             for ws in websocket_connections_timeseries:
                 ws.send_text(data)
 
 
 def on_event_received_handler(_: qx.StreamConsumer, event: qx.EventData):
-    global websocket_connections, websocket_connections_events
+    global websocket_connections_events
 
-    if len(websocket_connections_events) + len(websocket_connections) > 0:
+    if len(websocket_connections_events) > 0:
         data = json.dumps(
             {
                 "id": event.id,
@@ -47,8 +43,6 @@ def on_event_received_handler(_: qx.StreamConsumer, event: qx.EventData):
         )
         for ws in websocket_connections_events:
             ws.send_text(data)
-        for ws in websocket_connections:
-            ws.send_text(data)
 
 
 # this is the handler for new data streams on the broker
@@ -58,36 +52,28 @@ def on_stream_received_handler(stream_consumer: qx.StreamConsumer):
     stream_consumer.events.on_data_received = on_event_received_handler
 
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    # Store the WebSocket connection for later use
+@app.websocket("/ws/timeseries")
+async def websocket_endpoint_timeseries(websocket: WebSocket):
     await websocket.accept()
-    print("Client connected to socket.")
-
-    if websocket.url.path.endswith("/events"):
-        print("Client connected to events socket")
-        websocket_connections_events.append(websocket)
-    elif websocket.url.path.endswith("/timeseries"):
-        print("Client connected to timeseries socket")
-        websocket_connections_timeseries.append(websocket)
-    else:
-        websocket_connections.append(websocket)
-
-    i = len(websocket_connections) + len(websocket_connections_events) + len(websocket_connections_timeseries)
-    print(F"{i} active connection{'s'[:i ^ 1]}")
-
+    websocket_connections_timeseries.append(websocket)
     try:
-        # Keep the connection open and wait for messages if needed
-        print("Keep the connection open and wait for messages if needed")
         while True:
             await websocket.receive_text()
     except WebSocketDisconnect:
-        print("Client disconnected.")
-    finally:
-        # Remove the connection from the list when it's closed
-        print("Removing client from connection list")
-        if websocket in websocket_connections:
-            websocket_connections.remove(websocket)
+        if websocket in websocket_connections_timeseries:
+            websocket_connections_timeseries.remove(websocket)
+
+
+@app.websocket("/ws/events")
+async def websocket_endpoint_events(websocket: WebSocket):
+    await websocket.accept()
+    websocket_connections_events.append(websocket)
+    try:
+        while True:
+            await websocket.receive_text()
+    except WebSocketDisconnect:
+        if websocket in websocket_connections_events:
+            websocket_connections_events.remove(websocket)
 
 
 # Quix client
@@ -103,8 +89,3 @@ topic_consumer.on_stream_received = on_stream_received_handler
 
 # subscribe to data arriving at the topic_consumer
 topic_consumer.subscribe()
-
-qx.App.run()
-
-
-
